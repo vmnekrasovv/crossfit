13d5cde1a459f03e291b1a0981b49c69977a6784
vmnekrasovv <vmnekrasov777@gmail.com>
2025-08-16 05:47:13 +0300

    Убрал backdrop у селектов, пофиксил select.js

--- app/js/select.js
+++ app/js/select.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   class Select {
+
+    static instances = []
+
     constructor(selector, options) {
 
-      this.$el = document.querySelector(selector)
+      this.$el = selector
       this.options = options
 
       this.#setup()
+
+      Select.instances.push(this)
     }
 
     #setup() {
@@ -16,15 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     clickHandler(event) { 
+
       const {type} = event.target.dataset;
 
-      if (type === 'input') {
-        this.toggle()
-      } else if (type === 'item') {
-        this.select(event)
-      } else if (type === 'backdrop') {
-        this.close()
-      }
+      if (type === 'input' || type === 'value') this.toggle();
+      else if (type === 'item') this.select(event);
+      else this.toggle();
+
     }  
 
     get isOpen() {
@@ -45,7 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
         
         this.$value.textContent = event.target.textContent;
 
-
         this.close()
       }
 
@@ -61,6 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
           this.isOpen ? this.close() : this.open()
         } else {
           el.classList.remove('open')
+          el.querySelector('.select__arrow').classList.remove('arrow-up')
         }
       });    
     }
@@ -74,9 +78,25 @@ document.addEventListener("DOMContentLoaded", () => {
       this.$el.classList.remove('open')
       this.$arrow.classList.remove('arrow-up')
     }
+
+    static closeAll() {
+      Select.instances.forEach(instance => instance.close())
+    }
+
   }
 
   document.querySelectorAll('.select').forEach(el => {
-      new Select('#' + el.id);
+      new Select(document.querySelector('#' + el.id))
   })
+
+  document.addEventListener('click', (event) => {
+    
+    const clickedInsideSelect = Select.instances.some(
+      select => select.$el.contains(event.target)
+    );
+
+    if(!clickedInsideSelect) Select.closeAll();
+
+  }) // click event
+
 });
